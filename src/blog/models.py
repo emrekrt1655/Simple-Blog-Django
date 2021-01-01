bef1399a81366ed7f1540d7ebd3ee605e9fa0750
Emre <emrekurtt1655@gmail.com>
2021-01-01 18:02:32 +0100

    added media ur l and root

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,7 +5,13 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length = 100)
-
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
+        
 class Post(models.Model):
     OPTIONS = (
         ('d', 'Draft'),
@@ -20,4 +26,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete= models.CASCADE)
     status = models.CharField(max_length=10, choices = OPTIONS, default = 'd')
     slug = models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
     
