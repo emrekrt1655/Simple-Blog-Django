e63a2653ff4c4ebbde4b0af4a38e657d83b2359c
Emre <emrekurtt1655@gmail.com>
2021-01-09 19:18:31 +0100

    added email

--- src/users/views.py
+++ src/users/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import redirect, render
 from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
     form = RegistrationForm(request.POST or None)
-
+    if request.user.is_authenticated:
+        messages.warning(request, "You already have an account!")
+        return redirect("blog:list")
     if form.is_valid():
         form.save()
+        name = form.cleaned_data["username"]
+        messages.success(request, f"Acoount created for {name}")
         return redirect("login")
+    
 
     context = {
         "form": form,
@@ -25,6 +31,7 @@ def profile(request):
     if u_form.is_valid() and p_form.is_valid():
         u_form.save()
         p_form.save()
+        messages.success(request, "Your profile has been updated!!")
         return redirect(request.path)
 
     context = {
